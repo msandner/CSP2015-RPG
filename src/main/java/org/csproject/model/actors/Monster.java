342e6195f5216017ff421d93d6f01477329652ab
Maximilian Staab <mst@conleos.com>
2015-09-14 12:17:40 +0200

    test for git push

---
+++ src/main/java/org/csproject/model/actors/Monster.java
@@ -0,0 +1,10 @@
+package org.csproject.model.actors;
+
+public abstract class Monster
+  extends BattleActors
+{
+  public Monster(String name, String type, int level)
+  {
+    super(name, type, level);
+  }
+}
