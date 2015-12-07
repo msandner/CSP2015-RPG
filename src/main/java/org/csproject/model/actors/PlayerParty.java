ad0fcc6560f29158202f34a37ed0186007b0b845
NxPaqu <nickpaqu1@yahoo.com>
2015-12-07 13:25:22 -0500

    Added inventory and methods to playerParty

--- src/main/java/org/csproject/model/actors/PlayerParty.java
+++ src/main/java/org/csproject/model/actors/PlayerParty.java
@@ -1,5 +1,7 @@
 package org.csproject.model.actors;
 
+import org.csproject.model.items.Item;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,12 +12,14 @@ public class PlayerParty {
     protected PlayerActor char1, char2, char3;
     protected int currency;
     List<PlayerActor> party = new ArrayList<>();
+    List<Item> inventory;
 
     public PlayerParty(PlayerActor char1, PlayerActor char2, PlayerActor char3, int currency){
         this.char1 = char1;
         this.char2 = char2;
         this.char3 = char3;
         this.currency = currency;
+        inventory = new ArrayList<>();
         setParty();
     }
 
@@ -45,6 +49,25 @@ public class PlayerParty {
 
     public int getPartySize() {
         return party.size();
+    }
+
+    public List<Item> getInventory(){
+        return inventory;
+    }
+
+    public void addItem(Item i){
+        inventory.add(i);
+    }
+
+    public Item getItem(String s){
+        for(Item i : inventory){
+            if(i.getItemName() == s){
+                Item item = i;
+                inventory.remove(i);
+                return item;
+            }
+        }
+        return null;
     }
 
     public boolean isEveryPlayerDead() {
